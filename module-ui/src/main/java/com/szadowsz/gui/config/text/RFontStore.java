65599cc53a13f0512ace9ff01bb9ce8bdcb33beb
zakski <zak.szadowski+git@gmail.com>
2024-12-27 14:26:46 +0000

    remove unnecessary files

--- module-ui/src/main/java/com/szadowsz/gui/config/text/RFontStore.java
+++ module-ui/src/main/java/com/szadowsz/gui/config/text/RFontStore.java
@@ -1,6 +1,6 @@
 package com.szadowsz.gui.config.text;
 
-import com.szadowsz.gui.component.oldbined.swing.CharsetStreamTranslator;
+import com.szadowsz.gui.component.bined.CharsetStreamTranslator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import processing.core.PApplet;
